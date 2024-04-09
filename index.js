2e741cc4ec0cac67d3df42923a500cfb8fdaf422
Giulio-Marra <giuliomarramail@gmail.com>
2024-04-09 20:55:53 +0200

    delete All

--- index.js
+++
@@ -1,7 +0,0 @@
-const searchInput = document.getElementById("searchInput");
-const searchBtn = document.getElementById("searchBtn");
-
-searchBtn.addEventListener("click", function () {
-  searchInput.value = "";
-  searchInput.classList.toggle("active");
-});
